a61b3c2ac55669385383bcace2cbe5cc437d6e72
TomasVotruba <tomas.vot@gmail.com>
2020-07-16 18:29:48 +0200

    open 8.2-dev

---
+++ tests/Flex/FlexLoader/MicroKernelTraitTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\FlexLoader\Tests\Flex\FlexLoader;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Container;
+use Symplify\FlexLoader\Tests\Flex\FlexLoader\Source\ExtraService;
+use Symplify\FlexLoader\Tests\Flex\FlexLoader\Source\MicroKernelTraitKernel;
+use Symplify\FlexLoader\Tests\Flex\FlexLoader\Source\SomeService;
+
+final class MicroKernelTraitTest extends TestCase
+{
+    public function test(): void
+    {
+        $microKernelTraitKernel = new MicroKernelTraitKernel('dev', true);
+        $microKernelTraitKernel->boot();
+
+        /** @var Container $container */
+        $container = $microKernelTraitKernel->getContainer();
+
+        $this->assertTrue($container->has(SomeService::class));
+        $this->assertTrue($container->has(ExtraService::class));
+        $this->assertTrue($container->hasParameter('default.parameter'));
+        $this->assertTrue($container->hasParameter('dev.environment.parameter'));
+        $this->assertSame('app_level_value', $container->getParameter('package_param_overridable_on_app_level'));
+    }
+}
